d49483343d184a10455ddfea7b04a1d84e5761b2
Tonye Atiegoba <tonyetgb@gmail.com>
2017-09-14 05:38:22 +0100

    Final Updates

--- js/loadposts.js
+++ js/loadposts.js
@@ -1,4 +1,4 @@
-//Loading Latest Stories
+//Loading Top Story
 $.getJSON(
   "https://spreadsheet.glitch.me/?key=1QrV3AKLkdjdLZjmJ-fkNz-mgrXDwfe3vT980FCYUafk",
   data => {
@@ -13,7 +13,7 @@ $.getJSON(
 
 function template(item) {
   return `
-  <div class="quote"> 
+  <div class="quote">
       <a href='${item["Story URL"]}'><img alt='Photo for story' class='img-responsive' src='${item["Image URL"]}'></a>
       <a href='${item["Story URL"]}'>${item.Title}</a>
       <p>${item.Descriptions}.</p>
@@ -41,18 +41,45 @@ function template_1(item) {
       <h1 class="featured-video">
         <a href="${item["Story URL"]}">${item.Title}</a>
       </h1>
-      <span class="pubdate">By ${item["Author Name"]}</span>
-      <span class="pubdate">${item["Publish Date"]}</span>
+      <span class="pubdate">By ${item["Author Name"]} | </span>
+      <span class="pubdate">${item["Publish Date"]} | </span>
       <span class="pubdate">${item["Media"]}</span>
       <img alt="Photo for story" class="img-responsive" src="${item["Image URL"]}">
-      <p class="lead-video"> ${item.Descriptions}</p> 
+      <p class="lead-video"> ${item.Descriptions}</p>
       <a href='${item["Story URL"]}' class="button button-circle">Read More</a>
     </div>
   `;
 }
 
-//Loading Root Causes Posts
+//Loading Contributors
 $.getJSON(
+  "https://spreadsheet.glitch.me/?key=1bjzYjbSKKCNNqWmtouzojw_tGiL-1R3SxuDV1Fd510M",
+  data => {
+    console.log(data)
+    let containerhtml = "";
+    data.forEach(item => {
+      containerhtml += template_2(item);
+    });
+    $("#contributors").html(containerhtml)
+  }
+);
+
+function template_2(item) {
+  return `
+  <div class="col-sm-4">
+    <div class="about-countries-wide">
+      <img id="img-circle" class="img-responsive country" src="img/dummy.png">
+      <h1 class="featured-contributor">
+        <a>${item["Author Name"]}</a>
+      </h1>
+      <span class="pubdate">${item["Media"]}</span></br>
+      <span class="pubdate">${item["Twitter"]}</span>
+      <p class="lead-video">${item.Bio}</p>
+    </div>
+  </div>
+  `;
+}
+/* Loading Next
   "https://spreadsheet.glitch.me/?key=14UwvMiLf5S-IYjdiFCXk42i6Ve3Cwy-2i0Tc8G6YAFQ",
   data => {
     console.log(data)
@@ -70,6 +97,6 @@ function template_2(item) {
       <span class='pubdate'>${item["Publish Date"]}</span>
       <a href='${item["Story URL"]}'>${item.Title}</a>
       <span class='byline'>By ${item["Author Name"]} </span>
-    </li>  
+    </li>
   `;
-}
+} */
